29e91650f34fff1cb014c4b44a34f683f3046723
sonidukkipati <sonichalasani@yahoo.com>
2018-02-15 16:55:36 -0800

    Initial commit

---
+++ src/main/java/SampleJenkins.java
@@ -0,0 +1,10 @@
+public class SampleJenkins {
+
+    public static void main(String[] args){
+
+        System.out.println("This is sampleMavenGitProject with Jenkins");
+        System.out.println("This is sampleMavenGitProject with Jenkins first time");
+        System.out.println("This is sampleMavenGitProject with Jenkins trying");
+
+    }
+}
